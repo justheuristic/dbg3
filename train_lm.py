d7ae7889c905c09a07f7c35156644b8ae8dba88f
Max Ryabinin <mryabinin0@gmail.com>
2021-05-02 17:27:15 +0300

    Update train_lm.py

--- train_lm.py
+++ train_lm.py
@@ -1,17 +1,26 @@
-import os
-from argparse import ArgumentParser
-from multiprocessing import cpu_count
+from dataclasses import dataclass, field
 from pathlib import Path
+from typing import Optional
 
-import datasets
 import torch
 import torch.nn as nn
-from hivemind import RemoteMixtureOfExperts, DHT
-from transformers import Trainer, TrainingArguments
-from transformers.data.data_collator import DataCollatorForLanguageModeling
-from transformers import AdamW, get_linear_schedule_with_warmup
-from transformers.models.albert import AlbertTokenizerFast
-from transformers.models.albert.modeling_albert import AlbertEmbeddings, AlbertMLMHead, AlbertConfig, MaskedLMOutput
+from datasets import load_from_disk
+from hivemind import RemoteSwitchMixtureOfExperts, DHT
+from torch_optimizer import Lamb
+from transformers import AdamW, get_linear_schedule_with_warmup, DataCollatorForLanguageModeling, HfArgumentParser, \
+    Trainer, TrainingArguments, AlbertTokenizerFast, AlbertConfig
+from transformers.models.albert.modeling_albert import AlbertEmbeddings, AlbertMLMHead, MaskedLMOutput
+from transformers.optimization import get_linear_schedule_with_warmup
+
+
+@dataclass
+class DatasetArguments:
+    dataset_path: Optional[str] = field(default='.', metadata={"help": "Path to the dataset"})
+
+    tokenizer_path: Optional[str] = field(default='.', metadata={"help": "Path to the dataset"})
+
+    config_path: Optional[str] = field(default='.', metadata={"help": "Path to the dataset"})
+    cache_dir: Optional[str] = field(default='.', metadata={"help": "Path to the cache"})
 
 
 class TrainerModel(nn.Module):
@@ -26,10 +35,10 @@ class TrainerModel(nn.Module):
 
         self.mixture = nn.ModuleList(
             [
-                RemoteMixtureOfExperts(in_features=expert_dim, grid_size=grid_size, dht=dht,
-                                       k_best=3, k_min=2, forward_timeout=5, backward_timeout=5,
-                                       timeout_after_k_min=1, detect_anomalies=True,
-                                       uid_prefix='expert.')
+                RemoteSwitchMixtureOfExperts(in_features=expert_dim, grid_size=grid_size, dht=dht,
+                                             k_best=1, k_min=0, forward_timeout=2, backward_timeout=2,
+                                             timeout_after_k_min=1, detect_anomalies=True,
+                                             uid_prefix='expert.')
                 for _ in range(num_moe_blocks)])
 
         self.lm_head = AlbertMLMHead(self.config)
@@ -52,72 +61,53 @@ class TrainerModel(nn.Module):
         batch_size, seq_len = input_ids.size()
         embeddings = self.embedding_hidden_mapping_in(self.embeddings(input_ids))
 
+        loss_per_mixture = []
+
         for layer in self.mixture:
             # reshape and reshape back for token-level experts
             embeddings = embeddings.view(batch_size * seq_len, -1)
-            embeddings = layer(embeddings)
+            embeddings, balancing_loss = layer(embeddings)
+            loss_per_mixture.append(balancing_loss)
             embeddings = embeddings.view(batch_size, seq_len, -1)
+
         prediction_scores = self.lm_head(embeddings)
 
         masked_lm_loss = None
         if labels is not None:
             masked_lm_loss = nn.functional.cross_entropy(prediction_scores.view(-1, self.config.vocab_size),
                                                          labels.view(-1))
+            total_loss = masked_lm_loss + torch.stack(loss_per_mixture).sum()
 
         if not return_dict:
             output = (prediction_scores, embeddings)
             return ((masked_lm_loss,) + output) if masked_lm_loss is not None else output
 
         return MaskedLMOutput(
-            loss=masked_lm_loss,
+            loss=total_loss,
             logits=prediction_scores,
         )
 
 
-def main(args):
-    torch.set_num_threads(16)
-    tokenizer = AlbertTokenizerFast.from_pretrained('albert-base-v2')
+def main(dataset_args, args):
+    tokenizer = AlbertTokenizerFast.from_pretrained(dataset_args.tokenizer_path, cache_dir=dataset_args.cache_dir)
 
-    if not os.path.exists('tokenized_wikitext'):
-        wikitext = datasets.load_dataset('wikitext', 'wikitext-103-v1', cache_dir='.data_cache')
+    tokenized_datasets = load_from_disk(dataset_args.dataset_path)
 
-        def tokenize_function(examples):
-            # Remove empty lines
-            examples["text"] = [line for line in examples["text"] if len(line) > 0 and not line.isspace()]
-            return tokenizer(
-                examples["text"],
-                padding=False,
-                truncation=True,
-                max_length=256,
-                # We use this option because DataCollatorForLanguageModeling (see below) is more efficient when it
-                # receives the `special_tokens_mask`.
-                return_special_tokens_mask=True,
-            )
-
-        tokenized_wikitext = wikitext.map(
-            tokenize_function,
-            batched=True,
-            num_proc=cpu_count(),
-            remove_columns=["text"],
-        )
-
-        tokenized_wikitext.save_to_disk('tokenized_wikitext')
-    else:
-        tokenized_wikitext = datasets.load_from_disk('tokenized_wikitext')
-
-    collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
+    collator = DataCollatorForLanguageModeling(tokenizer, mlm=not args.lm)
 
     training_args = TrainingArguments(output_dir='lm', overwrite_output_dir=True,
                                       do_train=True, do_eval=True, do_predict=False,
                                       prediction_loss_only=True,
-                                      per_device_train_batch_size=1,
-                                      per_device_eval_batch_size=1, max_steps=10000, warmup_steps=4000,
+                                      per_device_train_batch_size=4,
+                                      per_device_eval_batch_size=4, max_steps=10000, warmup_steps=4000,
                                       gradient_accumulation_steps=1,
-                                      logging_first_step=True, max_grad_norm=1,
-                                      save_total_limit=5, seed=0, dataloader_num_workers=4, no_cuda=not args.cuda)
+                                      logging_first_step=True, logging_steps=10, max_grad_norm=1,
+                                      weight_decay=0.01,
+                                      save_total_limit=5, seed=0, dataloader_num_workers=0, no_cuda=not args.cuda)
 
-    grid_size = (2,)
-    dht = DHT(initial_peers=[args.init_peer], start=True, listen=False, max_workers=1, parallel_rpc=1, wait_timeout=0.1)
+    grid_size = (args.grid_size,)
+    dht = DHT(initial_peers=[args.init_peer], start=True, listen=False, max_workers=4, parallel_rpc=16,
+              wait_timeout=0.1)
 
     model = TrainerModel(tokenizer.vocab_size, args.hidden_dim, grid_size, dht)
 
@@ -133,11 +123,14 @@ def main(args):
         },
     ]
 
-    optimizer = AdamW(
+    optimizer = Lamb(
         optimizer_grouped_parameters,
         lr=training_args.learning_rate,
         betas=(training_args.adam_beta1, training_args.adam_beta2),
-        eps=training_args.adam_epsilon,
+        eps=1e-6,
+        weight_decay=training_args.weight_decay,
+        clamp_value=10000,
+        debias=True,
     )
 
     lr_scheduler = get_linear_schedule_with_warmup(
@@ -150,8 +143,9 @@ def main(args):
             model_checkpoint = torch.load(checkpoint_path)
             model.load_state_dict(model_checkpoint)
 
-    trainer = Trainer(model=model, args=training_args, train_dataset=tokenized_wikitext['train'],
-                      eval_dataset=tokenized_wikitext['validation'], tokenizer=tokenizer, data_collator=collator,
+    trainer = Trainer(model=model, args=training_args, train_dataset=tokenized_datasets,
+                      tokenizer=tokenizer,
+                      data_collator=collator,
                       optimizers=(optimizer, lr_scheduler))
 
     print('Starting training')
@@ -159,10 +153,12 @@ def main(args):
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument('--hidden-dim', type=int, default=1024, required=False, help='main dimension for expert_cls')
+    parser = HfArgumentParser((DatasetArguments,))
+    parser.add_argument('--hidden-dim', type=int, default=1024, help='main dimension for expert_cls')
+    parser.add_argument('--grid-size', type=int, default=8)
+    parser.add_argument('--lm', action='store_true')
     parser.add_argument('--init-peer')
     parser.add_argument('--restore-from-checkpoint')
     parser.add_argument('--cuda', action='store_true')
-    args = parser.parse_args()
-    main(args)
+    dataset_args, args = parser.parse_args_into_dataclasses()
+    main(dataset_args, args)
