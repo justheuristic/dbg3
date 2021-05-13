f71eb0c079c2d302ce2710cedcc241eed2952e20
Max Ryabinin <mryabinin0@gmail.com>
2021-05-13 20:55:20 +0300

    Increase expiration and timeout 10x

--- train_lm.py
+++ train_lm.py
@@ -66,11 +66,11 @@ class BaseTrainingArguments:
 @dataclass
 class AveragerArguments:
     averaging_expiration: float = field(
-        default=50.0,
+        default=5000.0,
         metadata={"help": "Averaging group will wait for stragglers for at most this many seconds"}
     )
     averaging_timeout: float = field(
-        default=300.0,
+        default=3000.0,
         metadata={"help": "Give up on averaging step after this many seconds"}
     )
     listen_on: str = field(
@@ -82,11 +82,11 @@ class AveragerArguments:
         metadata={"help": "Wait for at least this many seconds before fetching new collaboration state"}
     )
     max_refresh_period: float = field(
-        default=600,
+        default=60000,
         metadata={"help": "Wait for at most this many seconds before fetching new collaboration state"}
     )
     default_refresh_period: float = field(
-        default=300,
+        default=30000,
         metadata={"help": "Attempt to fetch collaboration state every this often until successful"}
     )
     expected_drift_peers: float = field(
@@ -106,7 +106,7 @@ class AveragerArguments:
         metadata={"help": "Maximum group size for all-reduce"}
     )
     metadata_expiration: float = field(
-        default=30,
+        default=3000,
         metadata={"help": "Peer's metadata will be removed if not updated in this many seconds"}
     )
 
