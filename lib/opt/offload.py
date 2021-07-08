c838174a552fb2d13654a9e05b1dcd7c7fd8e4c7
justheuristic <justheuristic@gmail.com>
2021-07-08 23:41:05 +0300

    hope it works

--- lib/opt/offload.py
+++ lib/opt/offload.py
@@ -20,12 +20,11 @@ class OffloadOptimizer(OptimizerWrapper):
         self.full_sync = full_sync
 
         with torch.no_grad():
-            self.original_params_per_group = tuple(group["params"] for group in param_groups)
-            self.offload_params_per_group = tuple(
+            self.offload_params_by_group = tuple(
                 [torch.nn.Parameter(torch.empty_like(param, device=offload_device), requires_grad=param.requires_grad)
                  for param in group["params"]] for group in param_groups)
 
-            for group, offload_params in zip(param_groups, self.offload_params_per_group):
+            for group, offload_params in zip(param_groups, self.offload_params_by_group):
                 for param, offload_param in zip(group['params'], offload_params):
                     offload_param.copy_(param, non_blocking=True)
                     if offload_param.grad is None:
@@ -33,31 +32,30 @@ class OffloadOptimizer(OptimizerWrapper):
                     if param.grad is not None:
                         offload_param.grad.copy_(param.grad, non_blocking=True)
 
-    @staticmethod
     @contextlib.contextmanager
-    def _replace_params(param_groups, replacement_params_per_group, *,
-                        sync_params_before: bool, sync_grads_before: bool,
-                        sync_params_after: bool, sync_grads_after: bool):
-        assert len(param_groups) == len(replacement_params_per_group)
-        original_params_per_group = [group["params"] for group in param_groups]
+    def _use_offloaded_params(self, *,
+                              sync_params_before: bool, sync_grads_before: bool,
+                              sync_params_after: bool, sync_grads_after: bool):
+        assert len(self.param_groups) == len(self.offload_params_by_group)
+        original_params_per_group = [group["params"] for group in self.param_groups]
         try:
             with torch.no_grad():
-                for original_params, replacement_params in zip(original_params_per_group, replacement_params_per_group):
+                for original_params, replacement_params in zip(original_params_per_group, self.offload_params_by_group):
                     for original_param, replacement_param in zip(original_params, replacement_params):
                         if sync_params_before:
                             replacement_param.copy_(original_param, non_blocking=True)
                         if sync_grads_before and original_param.grad is not None:
                             replacement_param.grad.copy_(original_param.grad, non_blocking=True)
 
-            for group, replacement_params in zip(param_groups, replacement_params_per_group):
+            for group, replacement_params in zip(self.param_groups, self.offload_params_by_group):
                 group["params"] = replacement_params
-            yield param_groups
+            yield self.param_groups
         finally:
-            for group, original_params in zip(param_groups, original_params_per_group):
+            for group, original_params in zip(self.param_groups, original_params_per_group):
                 group["params"] = original_params
 
             with torch.no_grad():
-                for original_params, replacement_params in zip(original_params_per_group, replacement_params_per_group):
+                for original_params, replacement_params in zip(original_params_per_group, self.offload_params_by_group):
                     for original_param, replacement_param in zip(original_params, replacement_params):
                         if sync_params_after:
                             original_param.copy_(replacement_param, non_blocking=True)
@@ -69,27 +67,23 @@ class OffloadOptimizer(OptimizerWrapper):
 
     def step(self, closure=None, *args, **kwargs):
         assert closure is None, "closure not supported in cpu offload mode"
-        with self._replace_params(self.param_groups, self.offload_params_per_group,
-                                  sync_params_before=self.full_sync, sync_grads_before=True,
-                                  sync_params_after=True, sync_grads_after=self.full_sync):
+        with self._use_offloaded_params(sync_params_before=self.full_sync, sync_grads_before=True,
+                                        sync_params_after=True, sync_grads_after=self.full_sync):
             return self.optim.step(*args, **kwargs)
 
     def zero_grad(self, set_to_none: bool = False, *args, **kwargs):
         if not self.full_sync:
             torch.optim.Optimizer.zero_grad(self, set_to_none)
-        with self._replace_params(self.param_groups, self.offload_params_per_group,
-                                  sync_params_before=self.full_sync, sync_grads_before=self.full_sync,
-                                  sync_params_after=self.full_sync, sync_grads_after=self.full_sync):
+        with self._use_offloaded_params(sync_params_before=self.full_sync, sync_grads_before=self.full_sync,
+                                        sync_params_after=self.full_sync, sync_grads_after=self.full_sync):
             return super().zero_grad(*args, set_to_none=False, **kwargs)
 
     def state_dict(self):
-        with self._replace_params(self.param_groups, self.offload_params_per_group,
-                                  sync_params_before=self.full_sync, sync_grads_before=self.full_sync,
-                                  sync_params_after=False, sync_grads_after=False):
+        with self._use_offloaded_params(sync_params_before=self.full_sync, sync_grads_before=self.full_sync,
+                                        sync_params_after=False, sync_grads_after=False):
             return self.optim.state_dict()
 
     def load_state_dict(self, state_dict: dict) -> None:
-        with self._replace_params(self.param_groups, self.offload_params_per_group,
-                                  sync_params_before=False, sync_grads_before=False,
-                                  sync_params_after=True, sync_grads_after=self.full_sync):
+        with self._use_offloaded_params(sync_params_before=False, sync_grads_before=False,
+                                        sync_params_after=True, sync_grads_after=self.full_sync):
             return self.optim.load_state_dict(state_dict)
