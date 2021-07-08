674b017525aca91cb4346c832fab0aa207bab391
justheuristic <justheuristic@gmail.com>
2021-07-08 20:49:09 +0300

    per-PG

--- lib/opt/offload.py
+++ lib/opt/offload.py
@@ -17,15 +17,14 @@ class OffloadOptimizer(OptimizerWrapper):
 
         self.offload_device = offload_device
         self.param_groups_main = param_groups
-        self.params_main = tuple(param for group in param_groups for param in group['params'])
         with torch.no_grad():
-            self.params_offload = tuple(torch.nn.Parameter(torch.empty_like(p, device=offload_device),
-                                                           requires_grad=p.requires_grad) for p in self.params_main)
-            for param_main, param_offload in zip(self.params_main, self.params_offload):
+            param_groups_offload = tuple(torch.nn.Parameter(
+                torch.empty_like(p, device=offload_device), requires_grad=p.requires_grad) for p in param_groups)
+            for param_main, param_offload in zip(param_groups, param_groups_offload):
                 param_offload.copy_(param_main, non_blocking=True)
                 if param_offload.grad is None:
                     param_offload.grad = torch.zeros_like(param_offload)
-        super().__init__(optim_cls(self.params_offload, *args, **kwargs))
+        super().__init__(optim_cls(param_groups_offload, *args, **kwargs))
 
     @property
     def param_groups(self):
@@ -45,29 +44,31 @@ class OffloadOptimizer(OptimizerWrapper):
     def step(self, closure=None, *args, **kwargs):
         assert closure is None, "closure not supported in cpu offload mode"
         with torch.no_grad():
-            for param_main, param_offload in zip(self.params_main, self.params_offload):
-                param_offload.copy_(param_main, non_blocking=True)
-                if param_main.grad is not None:
-                    param_offload.grad.copy_(param_main.grad, non_blocking=True)
+            for group, offload_group in zip(self.param_groups_main, self.optim.param_groups):
+                for param, offload_param in zip(group["params"], offload_group["params"]):
+                    offload_param.copy_(param, non_blocking=True)
+                    if param.grad is not None:
+                        offload_param.grad.copy_(param.grad, non_blocking=True)
 
         output = self.optim.step(*args, **kwargs)
 
         with torch.no_grad():
-            for param_main, param_offload in zip(self.params_main, self.params_offload):
-                param_main.copy_(param_offload, non_blocking=True)
+            for group, offload_group in zip(self.param_groups_main, self.optim.param_groups):
+                for param, offload_param in zip(group["params"], offload_group["params"]):
+                    param.copy_(offload_param, non_blocking=True)
         return output
 
     def zero_grad(self, set_to_none: bool = False, *args, **kwargs):
         with torch.no_grad():
-            for param_main in self.params_main:
-                if param_main.grad is not None:
-                    if set_to_none:
-                        param_main.grad = None
-                    else:
-                        if param_main.grad.grad_fn is not None:
-                            param_main.grad.detach_()
+            for group in self.param_groups_main:
+                for param in group:
+                    if param.grad is not None:
+                        if set_to_none:
+                            param.grad = None
                         else:
-                            param_main.grad.requires_grad_(False)
-                        param_main.grad.zero_()
-
+                            if param.grad.grad_fn is not None:
+                                param.grad.detach_()
+                            else:
+                                param.grad.requires_grad_(False)
+                            param.grad.zero_()
         return super().zero_grad(*args, set_to_none=False, **kwargs)
